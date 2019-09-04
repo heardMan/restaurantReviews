35bccf0f3f9a433800290bb71164a64cd10bb5a9
heardMan <markyheard@gmail.com>
2019-09-03 17:43:39 -0700

    Bug Fix: added
    updated image file paths

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant["main-photograph"]}`);
+    return (`./img/${restaurant["main-photograph"]}`);
   }
 
   /**
