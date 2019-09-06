ecd08290e2311d3c9fac02cd7be5d092ff9883bd
heardMan <markyheard@gmail.com>
2019-09-05 21:33:56 -0700

    Update: Service worker change
    - Udated Version number

--- sw.js
+++ sw.js
@@ -45,12 +45,44 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   const requestURL = new URL(event.request.url);
-  if(requestURL.pathname === 'restaurant.html'){
+  if(requestURL.pathname === '/restaurantReviews/restaurant.html'){
+    console.log('path matched');
     event.respondWith(
       caches.match('./restaurant.html')
-    )
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+      .catch(err=>console.log(err))
+    );
+    return;
   }
   console.log(requestURL);
+
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
